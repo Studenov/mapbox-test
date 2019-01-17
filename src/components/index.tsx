2c0ccc2f0f2dd7e74b6b181c0293366297da5e3f
Studenov <rstudenov@allmax.team>
2019-01-17 10:52:49 +0700

    app init

---
+++ src/components/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import logo from './src/static/images/logo.svg';
+
+export const Homepage = () => (
+  <div>
+    <p>Hello world!</p>
+    <img src={logo} alt="logo" title="logo" />
+  </div>
+);
